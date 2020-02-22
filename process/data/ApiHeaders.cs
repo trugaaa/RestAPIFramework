b29e7c1e9b7acce4b4fd8486378b3feb32e64181
trugaaa <andrey.trugaaa@gmail.com>
2020-02-23 00:43:51 +0200

    first commit

---
+++ process/data/ApiHeaders.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net.Mime;
+
+namespace RestAPIFrame.data
+{
+    public class ApiHeaders
+    {
+        public JsonObjectApi DataHeaders(string endpoint)
+        {
+            JsonObjectApi apiHeaders = new JsonObjectApi();
+            apiHeaders.PutAccept(MediaTypeNames.Application.Json);
+            apiHeaders.PutRequest(MediaTypeNames.Application.Json);
+            apiHeaders.PutEndpoint(endpoint);
+            return apiHeaders;
+        }
+        
+        public JsonObjectApi Id(string endpoint,int id)
+        {
+            JsonObjectApi apiHeaders = new ApiHeaders().DataHeaders(endpoint);
+            Dictionary<string, object>dictionary = new Dictionary<string, object>();
+            dictionary.Add("id",  id.ToString());
+            apiHeaders.PutHeaders(dictionary);
+            return apiHeaders;
+        }
+    }
+}
---
+++ process/data/Endpoints.cs
@@ -0,0 +1,7 @@
+﻿namespace RestAPIFrame.process.data
+{
+    public enum Endpoints
+    {
+        
+    }
+}
